3fe05b23e99bd225f9e2c565ab27bf47bd6c2486
tbarreto <tbarreto@intelix.biz>
2024-02-06 16:32:36 -0400

    Feat: data structures

---
+++ Data Structures and Algorithms/HashTables.py
@@ -0,0 +1,7 @@
+# You can use hash tables using dicts in python
+hash_table = dict()
+
+hash_table["Data_1"] = "Ejemplo de data 1"
+hash_table["Subdata_1"] = {"Data_1": "Ejemplo de data 1 de la subdata"}
+
+print(hash_table)
